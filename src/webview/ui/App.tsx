c71db554f054c7d4ccf6923b2eb9363734c678f0
pknowles <pknowles@users.noreply.github.com>
2026-03-15 13:13:42 -0700

    fix: ensure code columns fill webview panel vertically

--- src/webview/ui/App.tsx
+++ src/webview/ui/App.tsx
@@ -23,8 +23,8 @@ const MeldRoot: FC<{ children: React.ReactNode }> = ({ children }) => (
 	<div
 		style={{
 			display: "flex",
-			width: "100%",
-			height: "100%",
+			width: "100vw",
+			height: "100vh",
 			overflow: "hidden",
 			position: "relative",
 		}}
